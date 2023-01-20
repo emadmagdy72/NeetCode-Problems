ef05c2567f02fa17fe2e8a1d885c12328f8ef76e
emadmagdy72 <emadmagdy7272@gmail.com>
2023-01-20 06:27:39 +0200

    First Problem Contains dublicate in Arrays & Hashing

---
+++ Arrays & Hashing/217. Contains Duplicate.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        set1 = set()
+        for num in nums:
+            if num in set1:
+                return True
+            else:
+                set1.add(num)
+        return False
+
+        #.....Another Solutinos.....#
+        
+        # len1 = len(nums)
+        # len2 = len(set(nums))
+        # if(len1 == len2):
+        #     return False
+        # else:
+        #     return True
+
+
+
